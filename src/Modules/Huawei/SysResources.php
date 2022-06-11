fd448208b90da4d2c445d919fc4d61231790cff4
meklis <max.boyar.a@gmail.com>
2022-06-12 01:36:11 +0300

    Added module system resources

---
+++ src/Modules/Huawei/SysResources.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SwitcherCore\Modules\Huawei;
+
+use SnmpWrapper\Oid;
+use SwitcherCore\Modules\AbstractModule;
+
+class SysResources extends AbstractModule
+{
+    use InterfacesTrait;
+
+
+    public function run($params = [])
+    {
+        $id = $this->model->getExtraParamByName('sys_resources_id');
+        $oids = [];
+        foreach ($this->oids->getOidsByRegex('resources\..*') as $oid) {
+            $oids[] = Oid::init($oid->getOid() . ".{$id}");
+        }
+        $response = $this->formatResponse($this->snmp->get($oids));
+        $this->response = [
+            'cpu' => [
+                'util' => (int)$this->getResponseByName('resources.cpuUtil', $response)->fetchAll()[0]->getValue(),
+                '_temperature' => (float)($this->getResponseByName('resources.temperature', $response)->fetchAll()[0]->getValue()),
+                '_temperature_threshold' => (float)($this->getResponseByName('resources.temperatureThreshold', $response)->fetchAll()[0]->getValue()),
+            ],
+            'disk' => null,
+            'interfaces' => null,
+            'cards' => null,
+            'fans' => null,
+            'memory' => [
+                'util' =>  (int)$this->getResponseByName('resources.memUtil', $response)->fetchAll()[0]->getValue(),
+            ],
+        ];
+        return $this;
+    }
+
+    public function getPretty()
+    {
+        return $this->response;
+    }
+
+    public function getPrettyFiltered($filter = [])
+    {
+       return $this->response;
+    }
+
+}
