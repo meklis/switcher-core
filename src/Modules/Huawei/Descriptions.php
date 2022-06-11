73e6f8ecd0614382467974e771452a3b0cbc2c28
meklis <max.boyar.a@gmail.com>
2022-06-11 15:20:27 +0300

    Added new modules

---
+++ src/Modules/Huawei/Descriptions.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SwitcherCore\Modules\Huawei;
+
+use SwitcherCore\Modules\AbstractModule;
+use SwitcherCore\Modules\General\Switches\AbstractInterfaces;
+use SwitcherCore\Modules\General\Switches\FdbDot1Bridge;
+use SwitcherCore\Modules\Helper;
+
+class Descriptions extends \SwitcherCore\Modules\General\Switches\Descriptions
+{
+    use InterfacesTrait;
+
+}
