516654412a43f022cd1388db7dc15e5c61371be9
meklis <max.boyar.a@gmail.com>
2022-06-12 01:55:02 +0300

    Added support pvid module

--- src/Modules/General/Switches/PvidDot1q.php
+++ src/Modules/General/Switches/PvidDot1q.php
@@ -8,20 +8,24 @@ use SnmpWrapper\Oid;
 use SwitcherCore\Modules\Dlink\SwitchesPortAbstractModule;
 use SwitcherCore\Modules\Helper;
 
-abstract class PvidParser extends AbstractInterfaces
+abstract class PvidDot1q extends AbstractInterfaces
 {
     protected function formate() {
-        $indexes = $this->getInterfacesIds();
-        $response = [];
+        $indexes = [];
+        foreach ($this->getInterfacesIds() as $index) {
+            $indexes[$index['_snmp_id']] = [
+                'interface' => $index,
+                'pvid' => null,
+            ];
+        }
         foreach ($this->response as $oid_name => $wrappedResponse) {
             foreach ($wrappedResponse->fetchAll() as $resp) {
                 $port_index = Helper::getIndexByOid($resp->getOid());
-                $metric_name = str_replace(['dot1q_'], '', Helper::fromCamelCase($oid_name));
-                $response[$port_index][$metric_name] = $resp->getValue();
-                $response[$port_index]['interface'] = $this->parseInterface($port_index);
+                if(!isset($indexes[$port_index])) continue;
+                $indexes[$port_index]['pvid'] = $resp->getValue();
             }
         }
-        return array_values($response);
+        return array_values($indexes);
     }
     function getPretty()
     {
