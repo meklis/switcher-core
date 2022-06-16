fe8ae283527cc16d4461e2173c20fc2a4a590737
meklis <max.boyar.a@gmail.com>
2022-06-17 02:16:32 +0300

    Added support Edgecore ES3552M

---
+++ src/Modules/Edgecore/CableDiag.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace SwitcherCore\Modules\Edgecore;
+
+use SnmpWrapper\Oid;
+use SnmpWrapper\Request\PoollerRequest;
+use SwitcherCore\Exceptions\IncompleteResponseException;
+use SwitcherCore\Modules\AbstractModule;
+use SwitcherCore\Modules\General\Switches\AbstractInterfaces;
+use SwitcherCore\Modules\General\Switches\FdbDot1Bridge;
+use SwitcherCore\Modules\Helper;
+
+class CableDiag extends AbstractInterfaces
+{
+    use InterfacesTrait;
+
+    public function run($params = [])
+    {
+        $diagPorts = $this->getDiagPorts($params);
+        $interfaces = $this->getInterfacesIds();
+        $RESPONSES = [];
+        $actionOid = $this->oids->getOidByName('cable_diag.action')->getOid();
+        foreach ($diagPorts as $snmpId => $countPairs) {
+                //Start diag
+                $response = $this->formatResponse($this->snmp->set(
+                    Oid::init($actionOid ,
+                        false,
+                        PoollerRequest::TypeIntegerValue,
+                        $snmpId
+                    )));
+                if (!isset($response['cable_diag.action'])) {
+                    throw new \SNMPException("No response from device");
+                } elseif ($response['cable_diag.action']->error()) {
+                    throw new \SNMPException($response['cable_diag.action']->error());
+                }
+        }
+        sleep(1);
+        foreach ($diagPorts as $snmpId => $countPairs) {
+            //Prepare oids
+            if(!$countPairs == 4) {
+                $oids = [
+                    Oid::init($this->oids->getOidByName('cable_diag.resultPair1Status')->getOid().".{$snmpId}"),
+                    Oid::init($this->oids->getOidByName('cable_diag.resultPair2Status')->getOid().".{$snmpId}"),
+                    Oid::init($this->oids->getOidByName('cable_diag.resultPair3Status')->getOid().".{$snmpId}"),
+                    Oid::init($this->oids->getOidByName('cable_diag.resultPair4Status')->getOid().".{$snmpId}"),
+                    Oid::init($this->oids->getOidByName('cable_diag.resultPair1Length')->getOid().".{$snmpId}"),
+                    Oid::init($this->oids->getOidByName('cable_diag.resultPair2Length')->getOid().".{$snmpId}"),
+                    Oid::init($this->oids->getOidByName('cable_diag.resultPair3Length')->getOid().".{$snmpId}"),
+                    Oid::init($this->oids->getOidByName('cable_diag.resultPair4Length')->getOid().".{$snmpId}"),
+                ];
+            } else {
+                $oids = [
+                    Oid::init($this->oids->getOidByName('cable_diag.resultPair1Status')->getOid().".{$snmpId}"),
+                    Oid::init($this->oids->getOidByName('cable_diag.resultPair2Status')->getOid().".{$snmpId}"),
+                    Oid::init($this->oids->getOidByName('cable_diag.resultPair1Length')->getOid().".{$snmpId}"),
+                    Oid::init($this->oids->getOidByName('cable_diag.resultPair2Length')->getOid().".{$snmpId}"),
+                ];
+            }
+
+            $response = $this->formatResponse($this->snmp->get($oids));
+
+            $pairs = [];
+            foreach ($response as $oidName => $resp) {
+                if($resp->error()) {
+                    throw new \Exception($resp->error());
+                }
+                $match = [];
+                if(!preg_match('/^cable_diag\.resultPair([0-9])(.*)$/', $oidName, $match)) {
+                    throw new \Exception("Error parse oid name");
+                }
+                $type = strtolower($match[2]);
+                $pair = strtolower($match[1]);
+                $pairs[$pair]['pair'] = (int)$pair;
+                $pairs[$pair][$type] = $type == 'length' ? (int)$resp->fetchOne()->getValue() : $resp->fetchOne()->getParsedValue();
+            }
+
+            $RESPONSES[] = [
+                'interface' => $interfaces[$snmpId],
+                'pairs' => array_values($pairs),
+            ];
+        }
+        $this->response = $RESPONSES;
+        return $this;
+    }
+
+    protected function getCountPairsByPort($port) {
+        $pairs = $this->model->getExtraParamByName('count_pairs');
+        if(in_array($port, $this->model->getExtraParamByName('overwrite_pairs')['ports'])) {
+            $pairs = $this->model->getExtraParamByName('overwrite_pairs')['count_pairs'];
+        }
+        return $pairs;
+    }
+
+    protected function getDiagPorts($params) {
+        $forDiagPorts = [];
+        if ($params['interface']) {
+            $interface = $this->parseInterface($params['interface']);
+            $forDiagPorts[$interface['_snmp_id']] = $this->getCountPairsByPort();
+        } else {
+            $interfaces = $this->getInterfacesIds();
+            if (!$this->model->getExtraParamByName('diag_linkup')) {
+                $oid = Oid::init($this->oids->getOidByName('if.OperStatus')->getOid());
+                $response = $this->formatResponse($this->snmp->walk([$oid]));
+                $operStatus = $this->getResponseByName('if.OperStatus', $response);
+                if($operStatus->error()) {
+                    throw new \Exception($operStatus->error());
+                }
+                foreach ($operStatus->fetchAll() as $status) {
+                    if(!isset($interfaces[Helper::getIndexByOid($status->getOid())])) continue;
+                    if($status->getParsedValue() == 'Up') continue;
+                    $forDiagPorts[Helper::getIndexByOid($status->getOid())] = $this->getCountPairsByPort(Helper::getIndexByOid($status->getOid()));
+                }
+            }
+        }
+        return $forDiagPorts;
+    }
+
+    public function getPretty()
+    {
+        if(count($this->response) == 1) {
+            return $this->response[0];
+        } else if(count($this->response) == 0) {
+            return  null;
+        } else {
+            return  $this->response;
+        }
+    }
+
+    public function getPrettyFiltered($filter = [])
+    {
+        return $this->response;
+    }
+}
