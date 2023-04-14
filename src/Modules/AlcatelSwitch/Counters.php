6c5cf81517d70aa6736bb028eaf7fe088ac0f783
meklis <max.boyar.a@gmail.com>
2023-04-14 21:41:39 +0300

    Added alcatel switches

---
+++ src/Modules/AlcatelSwitch/Counters.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SwitcherCore\Modules\AlcatelSwitch;
+
+use SwitcherCore\Modules\AbstractModule;
+use SwitcherCore\Modules\General\Switches\AbstractInterfaces;
+use SwitcherCore\Modules\General\Switches\FdbDot1Bridge;
+use SwitcherCore\Modules\Helper;
+
+class Counters extends \SwitcherCore\Modules\General\Switches\Counters
+{
+    use InterfacesTrait;
+
+}
