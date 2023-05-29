d0b96e6713e40601ce88e9dbe921504d537e7fe7
meklis <max.boyar.a@gmail.com>
2023-05-29 22:43:05 +0300

    Try some fix

--- src/Modules/ZTE/C300Series/PonPortsList.php
+++ src/Modules/ZTE/C300Series/PonPortsList.php
@@ -4,31 +4,26 @@
 namespace SwitcherCore\Modules\ZTE\C300Series;
 
 
-
-
 use SwitcherCore\Modules\ZTE\ModuleAbstract;
 
 class PonPortsList extends ModuleAbstract
 {
     public function run($params = [])
     {
-        $response  = [];
-        $cards = $this->getModule('card_list')->run()->getPretty();
+        $response = [];
+        $cards = $this->getCardListWithStatuses();
         foreach ($cards as $card) {
-            if(!$card['technology']) continue;
-            for($port = 1; $port <= $card['num_ports']; $port++) {
-                try {
-                    $response[] = $this->parseInterface("{$card['technology']}-olt_{$card['shelf']}/{$card['slot']}/{$port}");
-                } catch (\Exception $e) {
-                    if(strpos("not in service card", $e->getMessage()) === false) {
-                        throw $e;
-                    }
-                }
+            if ($card['oper_status'] !== 'inService') continue;
+            if (!$card['technology']) continue;
+            for ($port = 1; $port <= $card['num_ports']; $port++) {
+                $response[] = $this->parseInterface("{$card['technology']}-olt_{$card['shelf']}/{$card['slot']}/{$port}");
+
             }
         }
         $this->response = $response;
-        return  $this;
+        return $this;
     }
+
     public function getPretty()
     {
         return $this->response;
