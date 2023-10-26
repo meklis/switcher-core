4cf39379264e7682b23efaa0cc010bbfeccdefd4
meklis <max.boyar.a@gmail.com>
2023-10-27 01:39:21 +0300

    Some fixes on HuaweiOLT

--- src/Modules/HuaweiOLT/InterfacesList.php
+++ src/Modules/HuaweiOLT/InterfacesList.php
@@ -35,30 +35,15 @@ class InterfacesList extends HuaweiOLTAbstractModule
 
     function getPretty()
     {
-        $data = $this->getResponseByName('ont.config.sn',$this->response);
-        $response = [];
-        foreach ($data->fetchAll() as $d) {
-            $xid = Helper::getIndexByOid($d->getOid(),1) . "." . Helper::getIndexByOid($d->getOid());
-            $iface = $this->parseInterface($xid);
-            $iface['_sn_hex'] = $d->getHexValue();
-            $response[] = $iface;
-        }
-        return $response;
+       return $this->response;
     }
 
 
     public function run($filter = [])
     {
-        $oid = $this->oids->getOidByName('ont.config.sn');
-        if($filter['interface']) {
-            $iface = $this->parseInterface($filter['interface']);
-            $oid = $oid->getOid() . "." . $iface['xid'];
-            $this->response = $this->formatResponse($this->snmp->get([Oid::init($oid)]));
-        } else {
-            $oid = $oid->getOid();
-            $this->response = $this->formatResponse($this->snmp->walk([Oid::init($oid)]));
-        }
-        return $this;
+         $this->response = array_values($this->getInterfaces());
+
+         return $this;
     }
 
 }
