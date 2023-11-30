6cae3fb747a95df54756e415ca5fe4ea5e66b0f2
meklis <max.boyar.a@gmail.com>
2023-11-30 20:58:39 +0200

    examples

--- src/Modules/Edgecore/ClearCounters.php
+++ src/Modules/Edgecore/ClearCounters.php
@@ -8,7 +8,8 @@ use SwitcherCore\Switcher\Console\ConsoleInterface;
 
 class ClearCounters extends AbstractModule
 {
-    protected $status = false;
+
+    use InterfacesTrait;
 
     /**
      * @Inject
@@ -18,36 +19,23 @@ class ClearCounters extends AbstractModule
 
     function getPretty()
     {
-        return $this->status;
+        return true;
     }
 
     function getPrettyFiltered($filter = [])
     {
-        return $this->status;
+        return $this->getPretty();
     }
 
     public function run($params = [])
     {
         $this->status = false;
-        //clear counters ethernet 1/1
-        //var_dump($this->model->getPorts());
-        //$this->console->connect($this->device->getIp());
-        //$this->console->setAccess($this->device->getLogin(),$this->device->getPassword());
-        $this->console->connect($this->device->getIp());
-        if(!$this->console) {
-            throw new Exception("Module ClearCounters required console connection");
-        }
+        $ports = $this->getInterfacesIds();
+        print_r($this->console->exec('show line'));
 
-        //$this->console->exec('show line');
-        //$this->console->write($this->device->getLogin());
-        //$this->console->write($this->device->getPassword());
-        //$this->console->write('clear counters ethernet 1/3');
-        $this->status = true;
 
-        return $this;
-        for($p=1;$p<=$this->model->getPorts();$p++){
 
-        }
+
         return $this;
     }
 }
