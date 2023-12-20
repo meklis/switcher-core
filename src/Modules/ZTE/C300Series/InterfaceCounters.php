2535c2353e110cf424037dad4115813e0c881cab
meklis <max.boyar.a@gmail.com>
2023-12-20 14:53:57 +0200

    Fix

--- src/Modules/ZTE/C300Series/InterfaceCounters.php
+++ src/Modules/ZTE/C300Series/InterfaceCounters.php
@@ -40,7 +40,7 @@ class InterfaceCounters extends ModuleAbstract
             $interface = $this->parseInterface($params['interface']);
             if ($interface['type'] === 'ONU') {
                 $oids = array_map(function ($e) use ($interface) {
-                    return $e . "." . $interface['_xid'];
+                    return $e . "." . $interface['_xpon_id'];
                 }, $this->getOidsForOnts());
             } else {
                 $oids = array_map(function ($e) use ($interface) {
