d50cdd344167ba51ccf068c7140ec1f5c5cd7e1b
dmru <rootfromua@gmail.com>
2024-01-09 22:13:11 +0200

    pon_onts_opical cdata fd12 fd16

--- src/Modules/CData/OntOpticalInfoFD12.php
+++ src/Modules/CData/OntOpticalInfoFD12.php
@@ -10,17 +10,50 @@ use SnmpWrapper\Response\PoollerResponse;
 use SwitcherCore\Modules\AbstractModule;
 use SwitcherCore\Modules\Helper;
 use SwitcherCore\Switcher\Objects\WrappedResponse;
+use SwitcherCore\Switcher\Console\ConsoleInterface;
 
-class OntOpticalInfo extends CDataAbstractModule
+class OntOpticalInfoFD12 extends CDataAbstractModule
 {
     /**
      * @var WrappedResponse[]
      */
     protected $response = null;
 
+    /**
+     * @Inject
+     * @var ConsoleInterface
+     */
+    protected $console;
+
     function getRaw()
     {
         return $this->response;
+    }
+
+    public function getOltRxFromConsole($interface, $fromCache = true)
+    {
+        $rx_arr = [];
+        preg_match_all('!\d+!', $interface['name'], $matches);
+        $cache_this_result_name = 'olr_rx_arr_' . $matches[0][0] . '/' . $matches[0][1] . '/' . $matches[0][2];
+
+        if ($fromCache && $rx_arr = $this->getCache($cache_this_result_name, true)) {
+            return $rx_arr;
+        }
+
+        $f_s = $matches[0][0] . '/' . $matches[0][1];
+        $port_numb = $matches[0][2];
+        $this->console->exec("interface {$interface['pontype']} {$f_s}");
+        $resp = $this->console->exec("show port ddm-info {$port_numb} detail");
+        $lines = explode("\n", $resp);
+        for ($i = 0; $i < count($lines); $i++) {
+            $parts = preg_split('/\s+/', trim($lines[$i]));
+            if (count($parts) == 2) {
+                $rx_arr[$parts[0]] = $parts[1];
+            }
+        }
+
+        $this->setCache($cache_this_result_name, $rx_arr, 3600, true);
+        return $rx_arr;
     }
 
     /**
@@ -38,19 +71,23 @@ class OntOpticalInfo extends CDataAbstractModule
         }
         foreach ($responses as $r) {
             $oid = $this->oids->findOidById($r->getOid());
-            if ($oid->getName() === 'ont.distance' || $oid->getName() === 'pon.portOpticalRxOfOnu') {
+            if ($oid->getName() === 'ont.distance') {
                 $onuId = Helper::getIndexByOid($r->getOid());
             } else {
                 $onuId = Helper::getIndexByOid($r->getOid(), 2);
             }
             $interface = $this->parseInterface($onuId);
             $return[$onuId]['interface'] = $interface;
+
+            if (!isset($return[$onuId]['olt_rx'])) {
+                $rx_arr = $this->getOltRxFromConsole($interface);
+                $rx = (isset($rx_arr[$interface['onu_num']])) ? $rx_arr[$interface['onu_num']] : null;
+                $return[$onuId]['olt_rx'] = $rx;
+            }
+
             switch ($oid->getName()) {
                 case 'ont.opticalRx':
                     $return[$onuId]['rx'] = round((float)$r->getValue() / 100, 2);
-                    break;
-                case 'pon.portOpticalRxOfOnu':
-                    $return[$onuId]['olt_rx'] = round((float)$r->getValue() / 100, 2);
                     break;
                 case 'ont.opticalTx':
                     $return[$onuId]['tx'] = round((float)$r->getValue() / 100, 2);
@@ -74,7 +111,6 @@ class OntOpticalInfo extends CDataAbstractModule
         return $this->response;
     }
 
-
     /**
      * @param array $filter
      * @return $this|AbstractModule
@@ -93,9 +129,6 @@ class OntOpticalInfo extends CDataAbstractModule
         if ($loadOnly === null || in_array('rx', $loadOnly)) {
             $optical[] = $this->oids->getOidByName('ont.opticalRx');
         }
-        if ($loadOnly === null || in_array('olt_rx', $loadOnly)) {
-            $optical[] = $this->oids->getOidByName('pon.portOpticalRxOfOnu');
-        }
         if ($loadOnly === null || in_array('temp', $loadOnly)) {
             $optical[] = $this->oids->getOidByName('ont.opticalTemp');
         }
@@ -109,7 +142,7 @@ class OntOpticalInfo extends CDataAbstractModule
             $oids = [];
             foreach ($this->getAllOntsIds(true) as $id) {
                 foreach ($optical as $optId) {
-                    if ($optId->getName() === 'ont.distance' || $optId->getName() === 'pon.portOpticalRxOfOnu') {
+                    if ($optId->getName() === 'ont.distance') {
                         $oids[] = Oid::init("{$optId->getOid()}.$id");
                     } else {
                         $oids[] = Oid::init("{$optId->getOid()}.$id.0.0");
@@ -121,7 +154,7 @@ class OntOpticalInfo extends CDataAbstractModule
             $oids = [];
             foreach ($this->getOntIdsByInterface($filter['interface'], true) as $id) {
                 foreach ($optical as $optId) {
-                    if ($optId->getName() === 'ont.distance' || $optId->getName() === 'pon.portOpticalRxOfOnu') {
+                    if ($optId->getName() === 'ont.distance') {
                         $oids[] = Oid::init("{$optId->getOid()}.$id");
                     } else {
                         $oids[] = Oid::init("{$optId->getOid()}.$id.0.0");
