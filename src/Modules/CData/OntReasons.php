277c955045d3de321933029ad9a6f2fb1f542eb3
meklis <max.boyar.a@gmail.com>
2024-03-04 21:49:43 +0200

    Try to add dereg time

--- src/Modules/CData/OntReasons.php
+++ src/Modules/CData/OntReasons.php
@@ -58,6 +58,8 @@ class OntReasons extends CDataAbstractModule
             $oid = $this->oids->findOidById($poolerResponse->getOid());
             $responses[] = WrappedResponse::init($poolerResponse, $oid->getValues());
         }
+        $regTimeSeconds = null;
+        $onlineTimeSeconds = null;
         foreach ($responses as $r) {
             $wr = $r->fetchAll()[0];
             $oid = $this->oids->findOidById($wr->getOid());
@@ -66,12 +68,20 @@ class OntReasons extends CDataAbstractModule
             switch ($oid->getName()) {
                 case 'ont.lastRegSince': $return[$onuId]['last_reg'] =  date("Y-m-d H:i:s", time() - $wr->getValue());
                                          $return[$onuId]['last_reg_since'] = $wr->getValueAsTimeTicks(SnmpResponse::HUMANIZE_DURATION);
+                                         $regTimeSeconds = ['sec' => $wr->getValue(), 'onu_id' => $onuId];
+                                         break;
+                case 'ont.lastDownSince': $onlineTimeSeconds = ['sec' => $wr->getValue(), 'onu_id' => $onuId];
                                          break;
                 case 'ont.lastDownReason': $return[$onuId]['last_down_reason'] = $wr->getParsedValue(); break;
             }
         }
+        if($onlineTimeSeconds && $regTimeSeconds && $return[$onlineTimeSeconds['onu_id']]['status'] !== 'Online') {
+            $return[$onlineTimeSeconds['onu_id']]['last_dereg'] = date("Y-m-d H:i:s", time() - ($regTimeSeconds['sec'] - $onlineTimeSeconds['sec']));
+        }
+
         $ids = array_keys($issetIds);
         $return = array_filter($return, function ($e) use ($ids) {
+
           return in_array($e['interface']['id'], $ids);
         });
         return array_values($return);
@@ -91,6 +101,7 @@ class OntReasons extends CDataAbstractModule
     public function run($filter = [])
     {
         $optical[] = $this->oids->getOidByName('ont.lastRegSince');
+        $optical[] = $this->oids->getOidByName('ont.lastDownSince');
         $optical[] = $this->oids->getOidByName('ont.lastDownReason');
 
         if(!$filter['interface']) {
