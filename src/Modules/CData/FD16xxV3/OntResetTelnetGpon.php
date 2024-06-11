05fb11a5f7bf5b98785594062b8d86461ea12a7c
meklis <max.boyar.a@gmail.com>
2024-06-11 10:18:24 +0300

    Fixed

--- src/Modules/CData/FD16xxV3/OntResetTelnetGpon.php
+++ src/Modules/CData/FD16xxV3/OntResetTelnetGpon.php
@@ -30,14 +30,8 @@ class OntResetTelnetGpon extends CDataAbstractModuleFD16xxV3
         if(!$interface['id']) {
             throw new \Exception("Incorrect ONU number");
         }
-        if (!preg_match('/^(pon|xge|ge)([0-9])\/([0-9])\/([0-9]){1,}\:?([0-9]{1,3})?\/?([0-9]{1,3})?$/', $interface['name'], $m)) {
-            throw new \Exception("Interface {$filter['interface']} not supported!");
-        }
-        if(count($m) < 5) {
-            throw new \Exception("Allow to reboot only ONTs");
-        }
         $this->_exe("interface gpon 0/0");
-        $this->_exe("ont restore factory {$m[4]} {$m[5]}");
+        $this->_exe("ont restore factory {$interface['_port']} {$interface['_onu']}");
         $this->response = true;
         return $this;
     }
