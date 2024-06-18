a097a19cdb42713f68229b960834b237436e8923
meklis <max.boyar.a@gmail.com>
2024-06-18 23:32:49 +0300

    Edit PON profiles

--- src/Modules/CData/GetProfiles.php
+++ src/Modules/CData/GetProfiles.php
@@ -34,9 +34,32 @@ class GetProfiles extends AbstractModule
      */
     public function run($filter = [])
     {
+
+        $prepared = [];
+        if(isset($filter['load_only']) && $filter['load_only']) {
+            $loadOnly = explode(",", $filter['load_only']);
+            if(in_array("line", $loadOnly)) {
+                $prepared[] = $this->oids->getOidByName('profile.line.name');
+            }
+            if(in_array("srv", $loadOnly)) {
+                $prepared[] = $this->oids->getOidByName('profile.srv.name');
+            }
+            if(in_array("dba", $loadOnly)) {
+                $prepared[] = $this->oids->getOidByName('profile.dba.name');
+            }
+            if(in_array("traffic", $loadOnly)) {
+                $prepared[] = $this->oids->getOidByName('profile.traffic.name');
+            }
+            if(in_array("sla", $loadOnly)) {
+                $prepared[] = $this->oids->getOidByName('profile.sla.name');
+            }
+        } else {
+            $prepared = $this->oids->getOidsByRegex('profile\..*');
+        }
+
         $oids = array_map(function ($e) {
             return Oid::init($e->getOid());
-        }, $this->oids->getOidsByRegex('profile\..*'));
+        }, $prepared);
         $this->response = $this->formatResponse($this->snmp->walkNext($oids));
         return $this;
     }
@@ -54,37 +77,65 @@ class GetProfiles extends AbstractModule
         ];
         if(isset($this->response['profile.dba.name']) && !$this->response['profile.dba.name']->error()) {
             foreach ($this->response['profile.dba.name']->fetchAll() as $d) {
-                $data['dba'][] = $this->convertHexToString($d->getHexValue());
+                $id = Helper::getIndexByOid($d->getOid());
+                $data['dba'][] = [
+                    'id' => $id,
+                    'name' => $this->convertHexToString($d->getHexValue()),
+                ];
             }
         }
         if(isset($this->response['profile.line.name']) && !$this->response['profile.line.name']->error()) {
             foreach ($this->response['profile.line.name']->fetchAll() as $d) {
-                $data['line'][] = $this->convertHexToString($d->getHexValue());
+                $id = Helper::getIndexByOid($d->getOid());
+                $data['line'][] = [
+                    'id' => $id,
+                    'name' => $this->convertHexToString($d->getHexValue()),
+                ];
             }
         }
         if(isset($this->response['profile.srv.name']) && !$this->response['profile.srv.name']->error()) {
             foreach ($this->response['profile.srv.name']->fetchAll() as $d) {
-                $data['srv'][] = $this->convertHexToString($d->getHexValue());
+                $id = Helper::getIndexByOid($d->getOid());
+                $data['srv'][] = [
+                    'id' => $id,
+                    'name' => $this->convertHexToString($d->getHexValue()),
+                ];
             }
         }
         if(isset($this->response['profile.traffic.name']) && !$this->response['profile.traffic.name']->error()) {
             foreach ($this->response['profile.traffic.name']->fetchAll() as $d) {
-                $data['traffic'][] = $this->convertHexToString($d->getHexValue());
+                $id = Helper::getIndexByOid($d->getOid());
+                $data['traffic'][] = [
+                    'id' => $id,
+                    'name' => $this->convertHexToString($d->getHexValue()),
+                ];
             }
         }
         if(isset($this->response['profile.alarm.name']) && !$this->response['profile.alarm.name']->error()) {
             foreach ($this->response['profile.alarm.name']->fetchAll() as $d) {
-                $data['alarm'][] = $this->convertHexToString($d->getHexValue());
+                $id = Helper::getIndexByOid($d->getOid());
+                $data['alarm'][] = [
+                    'id' => $id,
+                    'name' => $this->convertHexToString($d->getHexValue()),
+                ];
             }
         }
         if(isset($this->response['profile.sla.name']) && !$this->response['profile.sla.name']->error()) {
             foreach ($this->response['profile.sla.name']->fetchAll() as $d) {
-                $data['sla'][] = $this->convertHexToString($d->getHexValue());
+                $id = Helper::getIndexByOid($d->getOid());
+                $data['sla'][] = [
+                    'id' => $id,
+                    'name' => $this->convertHexToString($d->getHexValue()),
+                ];
             }
         }
         if(isset($this->response['profile.optical_alarm.name']) && !$this->response['profile.optical_alarm.name']->error()) {
             foreach ($this->response['profile.optical_alarm.name']->fetchAll() as $d) {
-                $data['optical_alarm'][] = $this->convertHexToString($d->getHexValue());
+                $id = Helper::getIndexByOid($d->getOid());
+                $data['optical_alarm'][] = [
+                    'id' => $id,
+                    'name' => $this->convertHexToString($d->getHexValue()),
+                ];
             }
         }
         return $data;
