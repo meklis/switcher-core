df4539a9773efc294a2d685503a1ce5aa7d2cf50
meklis <max.boyar.a@gmail.com>
2024-08-01 17:30:00 +0300

    Removed support ctrl_port_descr on FD16xx FW3

--- src/Modules/CData/FD16xxV3/PortDescriptionControl.php
+++ src/Modules/CData/FD16xxV3/PortDescriptionControl.php
@@ -14,20 +14,9 @@ class PortDescriptionControl extends CDataAbstractModuleFD16xxV3
 
     function setPortNameViaConsole($interface, $description)
     {
-        if (strlen($description) > 16) {
-            throw new \InvalidArgumentException("Port name. <S><Length 1~16>.");
-        }
-        preg_match_all('!\d+!', $interface['name'], $matches);
-        $f_s = $matches[0][0] . '/' . $matches[0][1];
-        $port_numb = $matches[0][2];
 
-        if(!isset($interface['pontype'])){
-            preg_match_all('![a-z]!', $interface['name'], $letters);
-            $interface['pontype'] = implode('', $letters[0]);
-        }
-
-        $this->console->exec("interface {$interface['pontype']} {$f_s}");
-        $response = $this->console->exec("port-name {$port_numb} {$description}");
+        $this->console->exec("interface gpon 0/0");
+        $response = $this->console->exec("port-desc {$interface['_port']} description {$description}");
 
         if (preg_match('/Command incomplete/', $response)) return false;
         if (preg_match('/Unknown command/', $response)) return false;
