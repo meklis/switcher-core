2a3e037b4a86bb827b7d3ed95c002ad7edabd6ee
meklis <max.boyar.a@gmail.com>
2024-10-16 00:04:52 +0300

    Added support Eltex switches

---
+++ src/Modules/EltexSwitch/RawConsoleCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace SwitcherCore\Modules\EltexSwitch;
+
+use Exception;
+use SwitcherCore\Switcher\Console\ConsoleInterface;
+use SwitcherCore\Modules\AbstractModule;
+
+class RawConsoleCommand extends AbstractModule
+{
+    /**
+     * @Inject
+     * @var ConsoleInterface
+     */
+    protected $console;
+
+    public function run($params = [])
+    {
+        if (!$this->console) {
+            throw new Exception("Module required telnet connection");
+        }
+        if (!isset($params['command'])) {
+            throw new \Exception("Command parameter is required");
+        }
+        $response = $this->console->exec($params['command']);
+        $this->response = [
+            'command' => $params['command'],
+            'output' => $response,
+            'success' => $this->validResponse($response),
+        ];
+        return $this;
+    }
+
+    protected function validResponse($response)
+    {
+        if (preg_match('/Incomplete/', $response)) return false;
+        if (preg_match('/bad parameter/', $response)) return false;
+        if (preg_match('/invalid/', $response)) return false;
+        if (preg_match('/Unrecognized/', $response)) return false;
+        return true;
+    }
+
+    function getPretty()
+    {
+        return $this->response;
+    }
+
+    function getPrettyFiltered($filter = [], $fromCache = false)
+    {
+        return $this->response;
+    }
+
+}
