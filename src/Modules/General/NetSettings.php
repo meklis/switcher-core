84783fccdc31d3d9e06e1f247c402732d901158c
meklis <max.boyar.a@gmail.com>
2024-10-21 22:48:35 +0300

    Change net_settings

--- src/Modules/General/NetSettings.php
+++ src/Modules/General/NetSettings.php
@@ -11,31 +11,50 @@ use SwitcherCore\Modules\Helper;
 class NetSettings extends AbstractModule
 {
     protected function formate() {
-        $response = [];
-        foreach ($this->getResponseByName('ip.netSettings')->fetchAll() as $num => $dt) {
-            $response['vlan_id'] = Helper::getIndexByOid($dt->getOid(), 4);
+        $response = [
+            'vlan_id'=> null,
+            'self_ip'=> null,
+            'self_mac_address'=> null,
+            'netmask'=> null,
+            'gateway_ip' => null,
+            'gateway_mac_address' => null,
+        ];
+        foreach ($this->getResponseByName('ip.netSettings')->fetchAll() as $dt) {
+            $idx = Helper::getIndexByOid($dt->getOid(), 4);
+            switch($idx) {
+                case 1: $response['self_ip'] = $dt->getValue(); break;
+                case 2: $response['vlan_id'] = $dt->getValue(); break;
+                case 3: $response['netmask'] = $dt->getValue(); break;
+            }
+        };
+        if(!$this->getResponseByName('sys.macAddr')->error()) {
+            $response['self_mac_address'] = $this->getResponseByName('sys.macAddr')->fetchOne()->getHexValue();
+        } elseif (!$this->getResponseByName('sys.macAddrV2')->error()) {
+            $response['self_mac_address'] = $this->getResponseByName('sys.macAddr')->fetchOne()->getHexValue();
+        }
+        $arps = [];
+        foreach ($this->getResponseByName('ip.arpTable')->fetchAll() as   $dt) {
+            $vlanId = Helper::getIndexByOid($dt->getOid(), 4);
             $ip =
                 Helper::getIndexByOid($dt->getOid(), 3) . "." .
                 Helper::getIndexByOid($dt->getOid(), 2) . "." .
                 Helper::getIndexByOid($dt->getOid(), 1) . "." .
                 Helper::getIndexByOid($dt->getOid())
             ;
-            switch ($num) {
-                case 0:
-                    $response['network'] = $ip;
-                    break;
-                case 1:
-                    $response['gateway_ip'] = $ip;
-                    $response['gateway_mac'] = $dt->getHexValue();
-                    break;
-                case 2:
-                    $response['self_ip'] = $ip;
-                    $response['self_mac'] = $dt->getHexValue();
-                    break;
-                case 3:
-                    $response['broadcast'] = $ip;
-                    break;
+
+            $idx = Helper::getIndexByOid($dt->getOid(), 5);
+
+            switch ($idx) {
+                case 1: $arps[$ip]['vlan_id'] = $dt->getValue(); break;
+                case 2: $arps[$ip]['mac_address'] = $dt->getHexValue(); break;
+                case 3: $arps[$ip]['ip'] = $dt->getValue(); break;
+                case 4: if(!in_array($dt->getValue(), [3,4])) unset($arps[$ip]); break;
             }
+        }
+        $response['_arps'] = array_values($arps);
+        if(count($arps) > 0) {
+            $response['gateway_ip'] = $response['_arps'][0]['ip'];
+            $response['gateway_mac_address'] = $response['_arps'][0]['mac_address'];
         }
         return $response;
     }
@@ -53,8 +72,13 @@ class NetSettings extends AbstractModule
     public function run($filter = [])
     {
         Helper::prepareFilter($filter);
-        $oidObjects = Oid::init($this->oids->getOidByName('ip.netSettings'));
-        $this->response = $this->formatResponse($this->snmp->walk([$oidObjects->getOid()]));
+        $oidObjects = [
+            Oid::init($this->oids->getOidByName('ip.arpTable')->getOid()),
+            Oid::init($this->oids->getOidByName('ip.netSettings')->getOid()),
+            Oid::init($this->oids->getOidByName('sys.macAddr')->getOid()),
+            Oid::init($this->oids->getOidByName('sys.macAddrV2')->getOid()),
+        ];
+        $this->response = $this->formatResponse($this->snmp->walk($oidObjects));
         return $this;
     }
 }
