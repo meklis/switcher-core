f9ba222def2428f08f7f2deeda39ad950a0e7108
meklis <max.boyar.a@gmail.com>
2025-03-24 20:31:58 +0200

    Fix interfaces trait

---
+++ src/Modules/HPESwitch/Rmon.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SwitcherCore\Modules\HPESwitch;
+
+use SwitcherCore\Modules\AbstractModule;
+use SwitcherCore\Modules\General\Switches\AbstractInterfaces;
+use SwitcherCore\Modules\General\Switches\FdbDot1Bridge;
+use SwitcherCore\Modules\Helper;
+
+class Rmon extends \SwitcherCore\Modules\General\Switches\Rmon
+{
+    use InterfacesTrait;
+
+}
